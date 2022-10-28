0612afa3cdf736a53d36c25457cc979b2d3a503c
katiejordan01 <kjordan62@gatech.edu>
2022-10-28 19:06:14 -0400

    Pop up color selector

--- main.js
+++ main.js
@@ -3,14 +3,23 @@ canvas.height = window.innerHeight
 canvas.width = window.innerWidth
 
 const ctx = canvas.getContext("2d")
-const {width, height} = canvas;
+//const {width, height} = canvas;
 
 let prevX = null
 let prevY = null
 
 ctx.lineWidth = 5
 
-let draw = false
+let draw = false;
+let clrDraw = true;
+let clrWidth = 200;
+let clrHeight = 150;
+
+let clrX;
+
+let clrY;
+let clrX2;
+let clrY2;
 
 let clrs = document.querySelectorAll(".clr")
 clrs = Array.from(clrs)
@@ -18,9 +27,27 @@ clrs.forEach(clr => {
     clr.addEventListener("click", () => {
         ctx.strokeStyle = clr.dataset.clr
     })
-    clr.addEventListener("mousedown", () => {
-        const {width, height} = canvas;
-        const gradientH = ctx.createLinearGradient(0, 0, 10, 0);
+    clr.addEventListener("mousedown", (e) => {
+
+        clrDraw = false;
+        clrX = e.clientX - clrWidth/2;
+        //680
+        clrY = e.clientY - clrHeight;
+        clrX2 = e.clientX + clrWidth/2;
+        clrY2 = e.clientY;
+        
+        div = document.createElement('div');
+        div.style.backgroundColor = "black";
+        div.style.position = "absolute";
+        div.style.left = "0px";
+        div.style.top = "0px";
+        div.style.width = "50px";
+        div.style.height = "50px";
+        div.setAttribute("id", "uniqueIdentifier");
+        document.getElementsByTagName('body')[0].appendChild(div);
+
+
+        const gradientH = ctx.createLinearGradient(clrX, 0, clrX2, 0);
         gradientH.addColorStop(0, "rgb(255, 0, 0)"); // red
         gradientH.addColorStop(1/6, "rgb(255, 255, 0)"); // yellow
         gradientH.addColorStop(2/6, "rgb(0, 255, 0)"); // green
@@ -29,14 +56,15 @@ clrs.forEach(clr => {
         gradientH.addColorStop(5/6, "rgb(255, 0, 255)");
         gradientH.addColorStop(1, "rgb(255, 0, 0)"); // red
         ctx.fillStyle = gradientH;
-        ctx.fillRect(0, 0, width, height);
-        const gradientV = ctx.createLinearGradient(0, 0, 0, height);
+        ctx.fillRect(clrX, clrY, clrWidth, clrHeight);
+        const gradientV = ctx.createLinearGradient(0, clrY, 0, clrY2);
         gradientV.addColorStop(0, "rgba(255, 255, 255, 1)"); // white
         gradientV.addColorStop(0.5, "rgba(255, 255, 255, 0)");
         gradientV.addColorStop(0.5, "rgba(0, 0, 0, 0)"); // transparent
         gradientV.addColorStop(1, "rgba(0, 0, 0, 1)"); // black
         ctx.fillStyle = gradientV;
-        ctx.fillRect(0, 0, width, height);
+        ctx.fillRect(clrX, clrY, clrWidth, clrHeight);
+
     })
 })
 
@@ -47,8 +75,17 @@ clearBtn.addEventListener("click", () => {
 })
 
 
-window.addEventListener("mousedown", (e) => draw = true)
-window.addEventListener("mouseup", (e) => draw = false)
+window.addEventListener("mousedown", (e) => {
+    if (clrDraw === true) {
+        draw = true
+    }
+    
+})
+window.addEventListener("mouseup", (e) => {
+    const element = document.getElementById('uniqueIdentifier');
+    element.remove();
+    draw = false
+})
 
 window.addEventListener("mousemove", (e) => {
     if(prevX == null || prevY == null || !draw){
