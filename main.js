e3831f5627edd19b7cebd3f5848eb81119b0bece
katiejordan01 <kjordan62@gatech.edu>
2022-10-28 17:54:13 -0400

    better UI

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ canvas.height = window.innerHeight
 canvas.width = window.innerWidth
 
 const ctx = canvas.getContext("2d")
+const {width, height} = canvas;
 
 let prevX = null
 let prevY = null
@@ -16,6 +17,26 @@ clrs = Array.from(clrs)
 clrs.forEach(clr => {
     clr.addEventListener("click", () => {
         ctx.strokeStyle = clr.dataset.clr
+    })
+    clr.addEventListener("mousedown", () => {
+        const {width, height} = canvas;
+        const gradientH = ctx.createLinearGradient(0, 0, 10, 0);
+        gradientH.addColorStop(0, "rgb(255, 0, 0)"); // red
+        gradientH.addColorStop(1/6, "rgb(255, 255, 0)"); // yellow
+        gradientH.addColorStop(2/6, "rgb(0, 255, 0)"); // green
+        gradientH.addColorStop(3/6, "rgb(0, 255, 255)");
+        gradientH.addColorStop(4/6, "rgb(0, 0, 255)"); // blue
+        gradientH.addColorStop(5/6, "rgb(255, 0, 255)");
+        gradientH.addColorStop(1, "rgb(255, 0, 0)"); // red
+        ctx.fillStyle = gradientH;
+        ctx.fillRect(0, 0, width, height);
+        const gradientV = ctx.createLinearGradient(0, 0, 0, height);
+        gradientV.addColorStop(0, "rgba(255, 255, 255, 1)"); // white
+        gradientV.addColorStop(0.5, "rgba(255, 255, 255, 0)");
+        gradientV.addColorStop(0.5, "rgba(0, 0, 0, 0)"); // transparent
+        gradientV.addColorStop(1, "rgba(0, 0, 0, 1)"); // black
+        ctx.fillStyle = gradientV;
+        ctx.fillRect(0, 0, width, height);
     })
 })
 
