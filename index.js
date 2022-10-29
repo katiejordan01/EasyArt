37d7a058912031ad7d8e7f93a89d1b476381abf9
katiejordan01 <kjordan62@gatech.edu>
2022-10-29 00:58:49 -0400

    attempt at paint bucket

--- index.js
+++ index.js
@@ -71,10 +71,88 @@ canvas.addEventListener('mousedown', (e) => {
     startY = e.clientY;
 });
 
-canvas.addEventListener('mouseup', e => {
+canvas.addEventListener('mouseup', (e) => {
     isPainting = false;
     ctx.stroke();
     ctx.beginPath();
 });
+canvas.addEventListener('click', function (evt) {
+    if (evt.detail === 3) {
+        pixelStack = [[evt.clientX, evt.clientY]];
+
+        while(pixelStack.length)
+        {
+        var newPos, x, y, pixelPos, reachLeft, reachRight;
+        newPos = pixelStack.pop();
+        x = newPos[0];
+        y = newPos[1];
+        
+        pixelPos = (y*canvas.width + x) * 4;
+        while(y-- >= 0 && matchStartColor(pixelPos))
+        {
+            pixelPos -= canvasWidth * 4;
+        }
+        pixelPos += canvasWidth * 4;
+        ++y;
+        reachLeft = false;
+        reachRight = false;
+        while(y++ < canvasHeight-1 && matchStartColor(pixelPos))
+        {
+            colorPixel(pixelPos);
+
+            if(x > 0)
+            {
+            if(matchStartColor(pixelPos - 4))
+            {
+                if(!reachLeft){
+                pixelStack.push([x - 1, y]);
+                reachLeft = true;
+                }
+            }
+            else if(reachLeft)
+            {
+                reachLeft = false;
+            }
+            }
+            
+            if(x < canvasWidth-1)
+            {
+            if(matchStartColor(pixelPos + 4))
+            {
+                if(!reachRight)
+                {
+                pixelStack.push([x + 1, y]);
+                reachRight = true;
+                }
+            }
+            else if(reachRight)
+            {
+                reachRight = false;
+            }
+            }
+                    
+            pixelPos += canvasWidth * 4;
+        }
+        }
+        context.putImageData(colorLayer, 0, 0);
+    }
+})
+
+function matchStartColor(pixelPos)
+{
+  var r = colorLayer.data[pixelPos];	
+  var g = colorLayer.data[pixelPos+1];	
+  var b = colorLayer.data[pixelPos+2];
+	
+  return (r == startR && g == startG && b == startB);
+}
+
+function colorPixel(pixelPos)
+{
+  colorLayer.data[pixelPos] = fillColorR;
+  colorLayer.data[pixelPos+1] = fillColorG;
+  colorLayer.data[pixelPos+2] = fillColorB;
+  colorLayer.data[pixelPos+3] = 255;
+}
 
 canvas.addEventListener('mousemove', draw);
